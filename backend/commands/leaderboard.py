921523749ee203b515035a599bb7276f7dea7380
Greg Dick <greg.dick@hudl.com>
2022-05-20 14:46:31 -0500

    Enhance leaderboard.

--- backend/commands/leaderboard.py
+++ backend/commands/leaderboard.py
@@ -1,8 +1,10 @@
 from backend import slack_util, configs, db
 import collections
 
-winrate_options = ['MATCHES', 'SETS', 'WINRATE']
-BAD_OPTION_MSG = 'The only valid options are `matches`, `sets`, or `winrate`.'
+matches_or_sets_options = ['MATCHES', 'SETS']
+sortby_options = ['PLAYED', 'WON', 'WINRATE']
+all_valid_options = ['ALL'] + matches_or_sets_options + sortby_options
+BAD_OPTION_MSG = 'The only options I recognize are `matches`, `sets`, `played`, `won`, `winrate`, and `all`.'
 
 
 def handles_message(lctx, command_object):
@@ -13,33 +15,57 @@ def handles_message(lctx, command_object):
 
 def handle_message(lctx, command_object):
     text = command_object.text
-    option = ''
+    options_text = ''
     if len(text) > 11:
-        option = text[11:].strip().upper()
-    if len(option) and option not in winrate_options:
+        options_text = text[11:].strip().upper()
+    options = options_text.split(' ')
+    if len(options_text) and len([x for x in options if x not in all_valid_options]):
         slack_util.post_message(lctx, BAD_OPTION_MSG, command_object.channel)
         return
 
-    sorted_winrates = get_leaderboard(lctx, option)
-    message = build_post(sorted_winrates)
+    matches_or_sets_o = [x for x in options if x in matches_or_sets_options]
+    if len(matches_or_sets_o) != 1:
+        matches_or_sets = 'MATCHES'
+    else:
+        matches_or_sets = matches_or_sets_o[0]
+
+    sortby_o = [x for x in options if x in sortby_options]
+    if len(sortby_o) != 1:
+        sortby = 'WON'
+    else:
+        sortby = sortby_o[0]
+
+    active_only = 'ALL' not in options
+
+    sorted_winrates = get_leaderboard(lctx, matches_or_sets, sortby, active_only)
+    message = build_post(sorted_winrates, matches_or_sets, sortby)
     slack_util.post_message(lctx, message, command_object.channel)
 
 
-def build_post(sorted_winrates):
+def build_post(sorted_winrates, matches_or_sets, sortby):
+    final_sort_prefix = 'matches_' if matches_or_sets.upper() == 'MATCHES' else 'games_'
     message = ''
     for player_name in list(sorted_winrates)[:10]:
         player_object = sorted_winrates[player_name]
-        message = message + "\n {}: {}-{} ({}-{}) {}%".format(player_name, player_object['matches_won'], player_object['matches_lost'], player_object['games_won'], player_object['games_lost'], player_object['winrate'])
+
+        if sortby.upper() == 'WON':
+            message += "\n {}: {}".format(player_name, player_object[final_sort_prefix+'won'])
+        elif sortby.upper() == 'PLAYED':
+            message += "\n {}: {}".format(player_name, player_object[final_sort_prefix+'total'])
+        elif sortby.upper() == 'WINRATE':
+            message += "\n {}: {}-{} {}%".format(player_name, player_object[final_sort_prefix+'won'], player_object[final_sort_prefix+'lost'], player_object[final_sort_prefix+'winrate'])
     return message
 
 
-def get_leaderboard(lctx, sortby, reverse_order=True):
-    if sortby.upper() == 'MATCHES' or sortby == '':
-        sortby = 'matches_won'
-    if sortby.upper() == 'SETS':
-        sortby = 'games_won'
-    if sortby.upper() == 'WINRATE':
-        sortby = 'winrate'
+def get_leaderboard(lctx, matches_or_sets, sortby, active_only, reverse_order=True):
+    final_sortby = ''
+    final_sort_prefix = 'matches_' if matches_or_sets.upper() == 'MATCHES' else 'games_'
+    if sortby.upper() == 'WON':
+        final_sortby = final_sort_prefix+'won'
+    elif sortby.upper() == 'PLAYED':
+        final_sortby = final_sort_prefix+'total'
+    elif sortby.upper() == 'WINRATE':
+        final_sortby = final_sort_prefix+'winrate'
 
     matches = db.get_matches(lctx.league_name)
     players = db.get_players(lctx.league_name)
@@ -52,7 +78,8 @@ def get_leaderboard(lctx, sortby, reverse_order=True):
             'matches_total': 0,
             'games_won': 0,
             'games_total': 0,
-            'name': player.name
+            'name': player.name,
+            'is_active': player.active
         }
 
     for match in matches:
@@ -85,22 +112,30 @@ def get_leaderboard(lctx, sortby, reverse_order=True):
     for player_id, player in player_dict.items():
         if sortby == 'winrate' and player['matches_total'] < 20:
             continue
+        if active_only and player['is_active'] != 1:
+            continue
         if player['games_total'] == 0:
             winrate_dict[player['name']] = {
                 'matches_won': 0,
                 'matches_lost': 0,
+                'matches_total': 0,
                 'games_won': 0,
                 'games_lost': 0,
-                'winrate': round(0, 2)
+                'games_total': 0,
+                'matches_winrate': round(0, 2),
+                'games_winrate': round(0, 2)
             }
         else:
             winrate_dict[player['name']] = {
                 'matches_won': player['matches_won'],
                 'matches_lost': player['matches_total'] - player['matches_won'],
+                'matches_total': player['matches_total'],
+                'matches_winrate': round((player['matches_won'] / player['matches_total']) * 100, 2),
                 'games_won': player['games_won'],
                 'games_lost': player['games_total'] - player['games_won'],
-                'winrate': round((player['games_won'] / player['games_total']) * 100, 2)
+                'games_total': player['games_total'],
+                'games_winrate': round((player['games_won'] / player['games_total']) * 100, 2)
             }
 
-    sorted_winrates = collections.OrderedDict(sorted(winrate_dict.items(), key=lambda x: x[1][sortby], reverse=reverse_order))
+    sorted_winrates = collections.OrderedDict(sorted(winrate_dict.items(), key=lambda x: x[1][final_sortby], reverse=reverse_order))
     return sorted_winrates
--- tests/commands/test_leaderboard.py
+++ tests/commands/test_leaderboard.py
@@ -24,7 +24,7 @@ class Test(TestCase):
             for i in range(1, 12):  # Odd number to get some byes
                 db.add_player(lctx.league_name, 'player{}{}'.format(g, i), 'Player {}{}'.format(g, i), g)
         for i in range(1, 13):
-            db.add_player(lctx.league_name, 'playerD{}'.format(i), 'Player D{}'.format(i), g)
+            db.add_player(lctx.league_name, 'playerD{}'.format(i), 'Player D{}'.format(i), 'D')
 
         match_making.create_matches_for_season(lctx.league_name, datetime.date(2022, 1, 3), 3, [], include_byes=True)
 
@@ -64,87 +64,121 @@ class Test(TestCase):
 
     def test_build_post(self):
         sorted_winrates = collections.OrderedDict({
-            'p1': {'matches_won': 50, 'matches_lost': 0, 'games_won': 100, 'games_lost': 0, 'winrate': 100.0},
-            'p2': {'matches_won': 45, 'matches_lost': 5, 'games_won': 90, 'games_lost': 10, 'winrate': 90.0},
-            'p3': {'matches_won': 40, 'matches_lost': 10, 'games_won': 80, 'games_lost': 20, 'winrate': 80.0},
-            'p4': {'matches_won': 35, 'matches_lost': 15, 'games_won': 70, 'games_lost': 30, 'winrate': 70.0},
-            'p5': {'matches_won': 30, 'matches_lost': 20, 'games_won': 60, 'games_lost': 40, 'winrate': 60.0},
-            'p6': {'matches_won': 25, 'matches_lost': 25, 'games_won': 50, 'games_lost': 50, 'winrate': 50.0},
-            'p7': {'matches_won': 20, 'matches_lost': 30, 'games_won': 40, 'games_lost': 60, 'winrate': 40.0},
-            'p8': {'matches_won': 15, 'matches_lost': 35, 'games_won': 30, 'games_lost': 70, 'winrate': 30.0},
-            'p9': {'matches_won': 10, 'matches_lost': 40, 'games_won': 20, 'games_lost': 80, 'winrate': 20.0},
-            'p10': {'matches_won': 5, 'matches_lost': 45, 'games_won': 10, 'games_lost': 90, 'winrate': 10.0},
-            'p11': {'matches_won': 0, 'matches_lost': 50, 'games_won': 0, 'games_lost': 100, 'winrate': 0.0}
+            'p1': {'matches_won': 50, 'matches_lost': 0, 'matches_total': 50, 'games_won': 100, 'games_lost': 0, 'games_total': 100, 'matches_winrate': 100.0, 'games_winrate': 100.1},
+            'p2': {'matches_won': 45, 'matches_lost': 5, 'matches_total': 50, 'games_won': 90, 'games_lost': 10, 'games_total': 100, 'matches_winrate': 90.0, 'games_winrate': 90.1},
+            'p3': {'matches_won': 40, 'matches_lost': 10, 'matches_total': 50, 'games_won': 80, 'games_lost': 20, 'games_total': 100, 'matches_winrate': 80.0, 'games_winrate': 80.1},
+            'p4': {'matches_won': 35, 'matches_lost': 15, 'matches_total': 50, 'games_won': 70, 'games_lost': 30, 'games_total': 100, 'matches_winrate': 70.0, 'games_winrate': 70.1},
+            'p5': {'matches_won': 30, 'matches_lost': 20, 'matches_total': 50, 'games_won': 60, 'games_lost': 40, 'games_total': 100, 'matches_winrate': 60.0, 'games_winrate': 60.1},
+            'p6': {'matches_won': 25, 'matches_lost': 25, 'matches_total': 50, 'games_won': 50, 'games_lost': 50, 'games_total': 100, 'matches_winrate': 50.0, 'games_winrate': 50.1},
+            'p7': {'matches_won': 20, 'matches_lost': 30, 'matches_total': 50, 'games_won': 40, 'games_lost': 60, 'games_total': 100, 'matches_winrate': 40.0, 'games_winrate': 40.1},
+            'p8': {'matches_won': 15, 'matches_lost': 35, 'matches_total': 50, 'games_won': 30, 'games_lost': 70, 'games_total': 100, 'matches_winrate': 30.0, 'games_winrate': 30.1},
+            'p9': {'matches_won': 10, 'matches_lost': 40, 'matches_total': 50, 'games_won': 20, 'games_lost': 80, 'games_total': 100, 'matches_winrate': 20.0, 'games_winrate': 20.1},
+            'p10': {'matches_won': 5, 'matches_lost': 45, 'matches_total': 50, 'games_won': 10, 'games_lost': 90, 'games_total': 100, 'matches_winrate': 10.0, 'games_winrate': 10.1},
+            'p11': {'matches_won': 0, 'matches_lost': 50, 'matches_total': 50, 'games_won': 0, 'games_lost': 100, 'games_total': 100, 'matches_winrate': 0.0, 'games_winrate': 0.1}
         })
-        result = leaderboard.build_post(sorted_winrates)
-        expected = '\n p1: 50-0 (100-0) 100.0%\n p2: 45-5 (90-10) 90.0%\n p3: 40-10 (80-20) 80.0%\n p4: 35-15 (70-30) 70.0%\n p5: 30-20 (60-40) 60.0%\n p6: 25-25 (50-50) 50.0%\n p7: 20-30 (40-60) 40.0%\n p8: 15-35 (30-70) 30.0%\n p9: 10-40 (20-80) 20.0%\n p10: 5-45 (10-90) 10.0%'
+        result = leaderboard.build_post(sorted_winrates, 'matches', 'winrate')
+        expected = '\n p1: 50-0 100.0%\n p2: 45-5 90.0%\n p3: 40-10 80.0%\n p4: 35-15 70.0%\n p5: 30-20 60.0%\n p6: 25-25 50.0%\n p7: 20-30 40.0%\n p8: 15-35 30.0%\n p9: 10-40 20.0%\n p10: 5-45 10.0%'
+        self.assertEqual(expected, result)
+
+        result = leaderboard.build_post(sorted_winrates, 'sets', 'winrate')
+        expected = '\n p1: 100-0 100.1%\n p2: 90-10 90.1%\n p3: 80-20 80.1%\n p4: 70-30 70.1%\n p5: 60-40 60.1%\n p6: 50-50 50.1%\n p7: 40-60 40.1%\n p8: 30-70 30.1%\n p9: 20-80 20.1%\n p10: 10-90 10.1%'
+        self.assertEqual(expected, result)
+
+        result = leaderboard.build_post(sorted_winrates, 'matches', 'won')
+        expected = '\n p1: 50\n p2: 45\n p3: 40\n p4: 35\n p5: 30\n p6: 25\n p7: 20\n p8: 15\n p9: 10\n p10: 5'
+        self.assertEqual(expected, result)
+
+        result = leaderboard.build_post(sorted_winrates, 'matches', 'played')
+        expected = '\n p1: 50\n p2: 50\n p3: 50\n p4: 50\n p5: 50\n p6: 50\n p7: 50\n p8: 50\n p9: 50\n p10: 50'
+        self.assertEqual(expected, result)
+
+        result = leaderboard.build_post(sorted_winrates, 'sets', 'won')
+        expected = '\n p1: 100\n p2: 90\n p3: 80\n p4: 70\n p5: 60\n p6: 50\n p7: 40\n p8: 30\n p9: 20\n p10: 10'
+        self.assertEqual(expected, result)
+
+        result = leaderboard.build_post(sorted_winrates, 'sets', 'played')
+        expected = '\n p1: 100\n p2: 100\n p3: 100\n p4: 100\n p5: 100\n p6: 100\n p7: 100\n p8: 100\n p9: 100\n p10: 100'
         self.assertEqual(expected, result)
 
     def test_get_leaderboard(self):
-        result = leaderboard.get_leaderboard(lctx, 'MaTcHeS')
-        short_result = collections.OrderedDict()
-        for top_player in list(result)[:4]:
-            short_result[top_player] = result[top_player]
-        top_expected = collections.OrderedDict({
-            'Player D1': {'matches_won': 22, 'matches_lost': 0, 'games_won': 77, 'games_lost': 55, 'winrate': 58.33},
-            'Player B1': {'matches_won': 20, 'matches_lost': 0, 'games_won': 70, 'games_lost': 20, 'winrate': 77.78},
-            'Player A1': {'matches_won': 19, 'matches_lost': 1, 'games_won': 67, 'games_lost': 3, 'winrate': 95.71},
-            'Player C1': {'matches_won': 18, 'matches_lost': 2, 'games_won': 68, 'games_lost': 42, 'winrate': 61.82}
-        })
-        self.assertEqual(top_expected, short_result)
+        result = leaderboard.get_leaderboard(lctx, 'MATCHES', 'WON', True)
+        last_result = 999999999
+        for top_player in list(result):
+            self.assertGreaterEqual(last_result, result[top_player]['matches_won'])
+            last_result = result[top_player]['matches_won']
+        self.assertIn('Player D1', list(result))
 
-        result = leaderboard.get_leaderboard(lctx, '')
-        short_result = collections.OrderedDict()
-        for top_player in list(result)[:4]:
-            short_result[top_player] = result[top_player]
-        self.assertEqual(top_expected, short_result)
+        result = leaderboard.get_leaderboard(lctx, 'MATCHES', 'PLAYED', True)
+        last_result = 999999999
+        for top_player in list(result):
+            self.assertGreaterEqual(last_result, result[top_player]['matches_total'])
+            last_result = result[top_player]['matches_total']
 
-        result = leaderboard.get_leaderboard(lctx, 'SeTs')
-        short_result = collections.OrderedDict()
-        for top_player in list(result)[:4]:
-            short_result[top_player] = result[top_player]
-        top_expected = collections.OrderedDict({
-            'Player D1': {'matches_won': 22, 'matches_lost': 0, 'games_won': 77, 'games_lost': 55, 'winrate': 58.33},
-            'Player B1': {'matches_won': 20, 'matches_lost': 0, 'games_won': 70, 'games_lost': 20, 'winrate': 77.78},
-            'Player C1': {'matches_won': 18, 'matches_lost': 2, 'games_won': 68, 'games_lost': 42, 'winrate': 61.82},
-            'Player A1': {'matches_won': 19, 'matches_lost': 1, 'games_won': 67, 'games_lost': 3, 'winrate': 95.71}
-        })
-        self.assertEqual(top_expected, short_result)
+        result = leaderboard.get_leaderboard(lctx, 'MATCHES', 'WINRATE', True)
+        last_result = 999999999.0
+        for top_player in list(result):
+            self.assertGreaterEqual(last_result, result[top_player]['matches_winrate'])
+            last_result = result[top_player]['matches_winrate']
 
-        result = leaderboard.get_leaderboard(lctx, 'wInRaTe')
-        short_result = collections.OrderedDict()
-        for top_player in list(result)[:4]:
-            short_result[top_player] = result[top_player]
-        top_expected = collections.OrderedDict({
-            'Player A1': {'matches_won': 19, 'matches_lost': 1, 'games_won': 67, 'games_lost': 3, 'winrate': 95.71},
-            'Player B1': {'matches_won': 20, 'matches_lost': 0, 'games_won': 70, 'games_lost': 20, 'winrate': 77.78},
-            'Player C1': {'matches_won': 18, 'matches_lost': 2, 'games_won': 68, 'games_lost': 42, 'winrate': 61.82},
-            'Player D1': {'matches_won': 22, 'matches_lost': 0, 'games_won': 77, 'games_lost': 55, 'winrate': 58.33}
-        })
-        self.assertEqual(top_expected, short_result)
+        result = leaderboard.get_leaderboard(lctx, 'SETS', 'WON', True)
+        last_result = 999999999
+        for top_player in list(result):
+            self.assertGreaterEqual(last_result, result[top_player]['games_won'])
+            last_result = result[top_player]['games_won']
+
+        result = leaderboard.get_leaderboard(lctx, 'SETS', 'PLAYED', True)
+        last_result = 999999999
+        for top_player in list(result):
+            self.assertGreaterEqual(last_result, result[top_player]['games_total'])
+            last_result = result[top_player]['games_total']
+
+        result = leaderboard.get_leaderboard(lctx, 'SETS', 'WINRATE', True)
+        last_result = 999999999.0
+        for top_player in list(result):
+            self.assertGreaterEqual(last_result, result[top_player]['games_winrate'])
+            last_result = result[top_player]['games_winrate']
+
+        db.set_active(lctx.league_name, 'playerD1', False)
+        result = leaderboard.get_leaderboard(lctx, 'MATCHES', 'WON', True)
+        self.assertNotIn('Player D1', list(result))
+
+        result = leaderboard.get_leaderboard(lctx, 'MATCHES', 'WON', False)
+        self.assertIn('Player D1', list(result))
 
     @patch.object(slack_util, 'post_message')
     def test_handle_message(self, mock_post_message):
-        msg = CommandMessage('leaderboard', 'channel', 'any_user', 'any_timestamp')
-        leaderboard.handle_message(lctx, msg)
-        mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, '')), 'channel')
+        cases = [
+            ['leaderboard', 'MATCHES', 'WON'],
+            ['leaderboard matches', 'MATCHES', 'WON'],
+            ['leaderboard sets', 'SETS', 'WON'],
+            ['leaderboard won', 'MATCHES', 'WON'],
+            ['leaderboard played', 'MATCHES', 'PLAYED'],
+            ['leaderboard winrate', 'MATCHES', 'WINRATE'],
+            ['leaderboard matches played', 'MATCHES', 'PLAYED'],
+            ['leaderboard matches won', 'MATCHES', 'WON'],
+            ['leaderboard matches winrate', 'MATCHES', 'WINRATE'],
+            ['leaderboard sets played', 'SETS', 'PLAYED'],
+            ['leaderboard sets won', 'SETS', 'WON'],
+            ['leaderboard sets winrate', 'SETS', 'WINRATE']]
 
-        mock_post_message.reset_mock()
-        msg = CommandMessage('leaderboard matches', 'channel', 'any_user', 'any_timestamp')
-        leaderboard.handle_message(lctx, msg)
-        mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, 'matches')), 'channel')
+        for case in cases:
+            mock_post_message.reset_mock()
+            msg = CommandMessage(case[0], 'channel', 'any_user', 'any_timestamp')
+            leaderboard.handle_message(lctx, msg)
+            mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, case[1], case[2], True), case[1], case[2]), 'channel')
 
-        mock_post_message.reset_mock()
-        msg = CommandMessage('leaderboard sets', 'channel', 'any_user', 'any_timestamp')
-        leaderboard.handle_message(lctx, msg)
-        mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, 'sets')), 'channel')
+        for player in db.get_players(lctx.league_name):
+            if player.grouping == 'D':
+                db.set_active(lctx.league_name, player.slack_id, False)
 
-        mock_post_message.reset_mock()
-        msg = CommandMessage('leaderboard winrate', 'channel', 'any_user', 'any_timestamp')
-        leaderboard.handle_message(lctx, msg)
-        mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, 'winrate')), 'channel')
+        for case in cases:
+            mock_post_message.reset_mock()
+            msg = CommandMessage(case[0], 'channel', 'any_user', 'any_timestamp')
+            leaderboard.handle_message(lctx, msg)
+            mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, case[1], case[2], True), case[1], case[2]), 'channel')
 
-        mock_post_message.reset_mock()
-        msg = CommandMessage('leaderboard winrate garbage', 'channel', 'any_user', 'any_timestamp')
-        leaderboard.handle_message(lctx, msg)
-        mock_post_message.assert_called_once_with(lctx, leaderboard.BAD_OPTION_MSG, 'channel')
+        for case in cases:
+            mock_post_message.reset_mock()
+            msg = CommandMessage(case[0] + ' all', 'channel', 'any_user', 'any_timestamp')
+            leaderboard.handle_message(lctx, msg)
+            mock_post_message.assert_called_once_with(lctx, leaderboard.build_post(leaderboard.get_leaderboard(lctx, case[1], case[2], False), case[1], case[2]), 'channel')
